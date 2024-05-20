d69f334509cceb5029abb0654277782fef2b4a6a
mikael englund <mikael.englund110@gmail.com>
2024-05-20 21:16:11 +0200

    finished lesson

--- 01-php-tags-printing-comments/index.php
+++ 01-php-tags-printing-comments/index.php
@@ -1,3 +1,11 @@
+<?php
+//this is from backend
+//anything within php tags are executed on the server! 
+echo 'Hello From PHP';
+echo '<br />';
+print'Hello from Print'; //print 
+echo 'Value one', 'Value two';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,19 +13,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Learn PHP From Scratch</title>
+    <title><?php echo 'Learn PHP From Scratch' ?></title>
 </head>
 
 <body class="bg-gray-100">
     <header class="bg-blue-500 text-white p-4">
         <div class="container mx-auto">
-            <h1 class="text-3xl font-semibold">Learn PHP From Scratch</h1>
+            <h1 class="text-3xl font-semibold"><?= 'Learn PHP From Scratch'?></h1>
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
         <div class="bg-white rounded-lg shadow-md p-6">
-            <h2 class="text-2xl font-semibold mb-4">Welcome To The Course</h2>
-            <p>In this course, you will learn the fundamentals of the PHP language</p>
+            <h2 class="text-2xl font-semibold mb-4"><?='Welcome To The Course'?></h2>
+            <p><?='In this course, you will learn the fundamentals of the PHP language'?></p>
         </div>
     </div>
 </body>
